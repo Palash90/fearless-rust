4f28f753c6d9e21813329752dccfb547f36b78f6
Palash <palash.kanti.kundu.90@gmail.com>
2025-11-04 20:10:11 +0530

    Complete the task

--- src/lib.rs
+++ src/lib.rs
@@ -1,9 +1,32 @@
-pub fn add(left: u64, right: u64) -> u64 {
-    left + right
+#[derive(PartialEq, Debug)]
+enum Scale {
+    Celsius,
+    Farhenheit,
 }
 
-pub fn sub(left: u64, right: u64) -> u64 {
-    left - right
+#[derive(PartialEq, Debug)]
+pub struct Temparature {
+    value: f64,
+    scale: Scale,
+}
+
+impl Temparature {
+    fn new(value: f64, scale: Scale) -> Self {
+        Temparature { value, scale }
+    }
+}
+
+pub fn temp_conv(t: Temparature) -> Temparature {
+    match t.scale {
+        Scale::Celsius => {
+            let value = (t.value * 1.8) + 32_f64;
+            Temparature::new(value, Scale::Farhenheit)
+        }
+        Scale::Farhenheit => {
+            let value = (t.value - 32_f64) * 5_f64 / 9_f64;
+            Temparature::new(value, Scale::Celsius)
+        }
+    }
 }
 
 #[cfg(test)]
@@ -11,14 +34,13 @@ mod tests {
     use super::*;
 
     #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
+    fn c_to_f_works() {
+        let source = Temparature::new(1_f64, Scale::Celsius);
+        let expected = Temparature::new(33.8, Scale::Farhenheit);
+
+        let derived = temp_conv(source);
+        assert_eq!(expected, derived);
     }
 
-    #[test]
-    fn sub_works() {
-        let result = sub(2, 2);
-        assert_eq!(0, result);
-    }
+    
 }
